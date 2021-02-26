3a02b10e29a027f87912086f4cfbaec0e76e4f57
yepeng <initsysctrl@163.com>
2021-02-26 21:25:46 +0800

    手写识别

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 '''
 Date: 2021-02-26 17:33:45
-LastEditTime: 2021-02-26 17:39:16
+LastEditTime: 2021-02-26 19:50:15
 Author: Ye-P
 Descripttion: 
 '''
+import matplotlib.pyplot as plt
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
 
 
 def fff(a: int):
     print(a)
 
 
-# fff('deded')
+fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 4))
+# print()
+plt.show()
