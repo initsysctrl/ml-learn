53efd8f2428797973c18e5d07bfa308929287ee1
yepeng <initsysctrl@163.com>
2021-03-01 02:51:45 +0800

    svm test

---
+++ svm_test.py
@@ -0,0 +1,6 @@
+'''
+Date: 2021-03-01 02:51:29
+LastEditTime: 2021-03-01 02:51:29
+Author: Ye-P
+Descripttion: 
+'''
--- test.py
+++ test.py
@@ -1,18 +1,32 @@
 '''
 Date: 2021-02-26 17:33:45
-LastEditTime: 2021-02-26 19:50:15
+LastEditTime: 2021-03-01 02:46:04
 Author: Ye-P
 Descripttion: 
 '''
 import matplotlib.pyplot as plt
-from sklearn import datasets, svm, metrics
-from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.animation as animation
+x = np.linspace(-2*np.pi, 2*np.pi, 100)
+y = np.sin(x)
+fig = plt.figure()
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.grid('-')
+line, = plt.plot(x, y, color='black')
 
 
-def fff(a: int):
-    print(a)
+def init():  # only required for blitting to give a clean slate.
+    line.set_ydata([np.nan] * len(x))
+    return line,
 
 
-fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 4))
-# print()
+def update(i):
+    line.set_ydata(np.sin(x + i / 100))  # update the data.
+    return line,
+
+
+ani = animation.FuncAnimation(
+    fig, update, init_func=init, interval=2, blit=True, save_count=50)
+
 plt.show()
